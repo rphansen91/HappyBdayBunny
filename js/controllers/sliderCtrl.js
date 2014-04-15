fe8d7672e7193f6911ca97fbedc452610259a037
Ryan P Hansen <rphansen91@gmail.com>
2014-04-15 12:06:18 -0700

    GH Pages

--- js/controllers/sliderCtrl.js
+++ js/controllers/sliderCtrl.js
@@ -3,32 +3,14 @@ function sliderCtrl($scope) {
 		{
 			src:'/img/home/wedding.jpg',
 			title:'HAPPY BIRTHDAY',
-			caption:'YOU ARE LOVED SO MUCH',
 			state:'greetings',
 			btn:'GREETINGS'
 		},
 		{
 			src:'/img/home/graduation.jpg',
 			title:'WHAT A GREAT YEAR',
-			caption:'LOOK BACK',
 			state:'photos',
 			btn:'PHOTOS'
 		}
 	];
-	$scope.currentIndex = 0;
-
-    $scope.setCurrentSlideIndex = function (index) {
-        $scope.currentIndex = index;
-    };
-
-    $scope.isCurrentSlideIndex = function (index) {
-       return $scope.currentIndex === index;
-    };
-    $scope.prevSlide = function () {
-        $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
-    };
-
-    $scope.nextSlide = function () {
-        $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
-    };
 };
