9eeb6ef205f82e8afd881b9a2a40cf69a9089367
Ryan P Hansen <rphansen91@gmail.com>
2014-04-14 20:26:02 -0700

    Animations

---
+++ js/controllers/greetingsCtrl.js
@@ -0,0 +1,49 @@
+function greetingsCtrl($scope){
+	$scope.messages = [
+		{
+			author:'Dennis',
+			img:'/img/greetings/dennis/dennis.jpg',
+			mes:''
+		},
+		{
+			author:'Ashley',
+			img:'/img/greetings/ashley/ashley.jpg',
+			mes:'Happy birthday to an amazing woman I am lucky to call Mom! I hope you have the best day, you deserve nothing less!  Wish we could be there to celebrate with you!'
+		},
+		{
+			author:'Ali',
+			img:'/img/greetings/ali/ali.jpg',
+			mes:''
+		},
+		{
+			author:'Ryan',
+			img:'/img/greetings/ryan/ryan.jpg',
+			mes:''
+		},
+		{
+			author:'Garrett',
+			img:'/img/greetings/garrett/garrett.jpg',
+			mes:'Happy birthday Benita! Hope it is a great one! Hope you can spend some time enjoying your new garden!!'
+		},
+		{
+			author:'Denise',
+			img:'/img/greetings/denise/denise.jpg',
+			mes:''
+		},
+		{
+			author:'David',
+			img:'/img/greetings/david/david.jpg',
+			mes:''
+		},
+		{
+			author:'Danny',
+			img:'',
+			mes:''
+		},
+		{
+			author:'Susan',
+			img:'/img/greetings/susan/susan.jpg',
+			mes:''
+		},
+	];
+};
---
+++ js/controllers/sliderCtrl.js
@@ -0,0 +1,18 @@
+function sliderCtrl($scope) {
+	$scope.images = [
+		{
+			src:'/img/home/wedding.jpg',
+			title:'HAPPY BIRTHDAY',
+			caption:'YOU ARE LOVED SO MUCH',
+			state:'greetings',
+			btn:'GREETINGS'
+		},
+		{
+			src:'/img/home/graduation.jpg',
+			title:'WHAT A GREAT YEAR',
+			caption:'LOOK BACK',
+			state:'photos',
+			btn:'PHOTOS'
+		}
+	];
+};
