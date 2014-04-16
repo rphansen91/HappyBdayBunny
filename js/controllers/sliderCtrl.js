a421358ee431f9d4b932a62c8b44bf6dbf632a5a
Ryan P Hansen <rphansen91@gmail.com>
2014-04-15 18:09:14 -0700

    img route1

--- js/controllers/sliderCtrl.js
+++ js/controllers/sliderCtrl.js
@@ -1,13 +1,13 @@
 function sliderCtrl($scope) {
 	$scope.slides = [
 		{
-			src:'/img/home/wedding.jpg',
+			src:'HappyBdayBunny/#/img/home/wedding.jpg',
 			title:'HAPPY BIRTHDAY',
 			state:'greetings',
 			btn:'GREETINGS'
 		},
 		{
-			src:'/img/home/graduation.jpg',
+			src:'HappyBdayBunny/#/img/home/graduation.jpg',
 			title:'WHAT A GREAT YEAR',
 			state:'photos',
 			btn:'PHOTOS'
