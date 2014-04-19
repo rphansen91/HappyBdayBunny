5a9ae71577b866c44a7c935526ce85cd792a4126
Ryan P Hansen <rphansen91@gmail.com>
2014-04-18 20:46:03 -0700

    Asset_url

--- js/controllers/greetingsCtrl.js
+++ js/controllers/greetingsCtrl.js
@@ -2,47 +2,42 @@ function greetingsCtrl($scope){
 	$scope.messages = [
 		{
 			author:'Dennis',
-			img:'/HappyBday/img/greetings/dennis/dennis.jpg',
+			img:'/HappyBdayBunny/img/greetings/dennis/dennis.jpg',
 			mes:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, cupiditate, facilis, ullam obcaecati iure amet nemo repellendus quis nesciunt dolorem eum vel inventore!'
 		},
 		{
 			author:'Ashley',
-			img:'/HappyBday/img/greetings/ashley/ashley.jpg',
+			img:'/HappyBdayBunny/img/greetings/ashley/ashley.jpg',
 			mes:'Happy birthday to an amazing woman I am lucky to call Mom! I hope you have the best day, you deserve nothing less!  Wish we could be there to celebrate with you!'
 		},
 		{
 			author:'Ali',
-			img:'/HappyBday/img/greetings/ali/ali.jpg',
+			img:'/HappyBdayBunny/img/greetings/ali/ali.jpg',
 			mes:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, cupiditate, facilis, ullam obcaecati iure amet nemo repellendus quis nesciunt dolorem eum vel inventore!'
 		},
 		{
 			author:'Ryan',
-			img:'/HappyBday/img/greetings/ryan/ryan.jpg',
+			img:'/HappyBdayBunny/img/greetings/ryan/ryan.jpg',
 			mes:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, cupiditate, facilis, ullam obcaecati iure amet nemo repellendus quis nesciunt dolorem eum vel inventore!'
 		},
 		{
 			author:'Garrett',
-			img:'/HappyBday/img/greetings/garrett/garrett.jpg',
+			img:'/HappyBdayBunny/img/greetings/garrett/garrett.jpg',
 			mes:'Happy birthday Benita! Hope it is a great one! Hope you can spend some time enjoying your new garden!!'
 		},
 		{
 			author:'Denise',
-			img:'/HappyBday/img/greetings/denise/denise.jpg',
+			img:'/HappyBdayBunny/img/greetings/denise/denise.jpg',
 			mes:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, cupiditate, facilis, ullam obcaecati iure amet nemo repellendus quis nesciunt dolorem eum vel inventore!'
 		},
 		{
 			author:'David',
-			img:'/HappyBday/img/greetings/david/david.jpg',
-			mes:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, cupiditate, facilis, ullam obcaecati iure amet nemo repellendus quis nesciunt dolorem eum vel inventore!'
-		},
-		{
-			author:'Danny',
-			img:'',
+			img:'/HappyBdayBunny/img/greetings/david/david.jpg',
 			mes:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, cupiditate, facilis, ullam obcaecati iure amet nemo repellendus quis nesciunt dolorem eum vel inventore!'
 		},
 		{
 			author:'Susan',
-			img:'/HappyBday/img/greetings/susan/susan.jpg',
+			img:'/HappyBdayBunny/img/greetings/susan/susan.jpg',
 			mes:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, cupiditate, facilis, ullam obcaecati iure amet nemo repellendus quis nesciunt dolorem eum vel inventore!'
 		},
 	];
